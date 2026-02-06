fe778fcdb0d73bcb81d4cd68f624f233e8da3945
SRIKANTH B <112770979+Sri2413@users.noreply.github.com>
2026-02-06 13:45:08 +0530

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -15,7 +15,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://taskorbit-2.onrender.com/api/auth/login",
         {
           email,
           password,
